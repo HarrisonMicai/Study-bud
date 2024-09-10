a634bef68b0c8b5e1d48bf11ae8fa77c5ff42ccc
[mharr234@uncc.edu] <152901394+HarrisonMicai@users.noreply.github.com>
2024-09-10 13:48:55 -0400

    1:04:25
    Database & Admin Panel

--- base/urls.py
+++ base/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     # Routes
     path('', views.home, name="home"),
-    path('room/', views.room, name="room"),
-   # path('room/<str:pk>/', views.room, name="room"),
+    path('room/<str:pk>/', views.room, name="room"),
    # path('create-room/', views.createRoom, name="create-room")
 ]
