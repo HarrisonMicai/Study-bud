8ea93f09a7129185eefa5aeb1c72c80ebbc98fb2
[mharr234@uncc.edu] <152901394+HarrisonMicai@users.noreply.github.com>
2024-09-17 19:53:13 -0400

    2:41:45
    Restricted Pages

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"), # 2:41:45
+     path('register/', views.registerPage, name="register"),
     # Routes
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
